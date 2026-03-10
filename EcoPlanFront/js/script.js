905a58d272f595eca3c768ebe58b0b29cde9f5a6
Clemerso Fresileben Troian <165335337+ClemersonFT@users.noreply.github.com>
2026-03-10 15:22:37 -0300

    Popup de login adicionado

--- EcoPlanFront/js/script.js
+++ EcoPlanFront/js/script.js
@@ -1,25 +1,36 @@
-const heroBg = document.querySelector(".hero-img");
+const heroimg = document.querySelector(".hero-img");
 const direcionamento = document.querySelector(".direcionamento");
-
-window.addEventListener("scroll", () => {
-
-    let scroll = window.scrollY;
-
-    let blur = 10 - scroll / 50;
-
-    if (blur < 0) blur = 0;
-
-    heroBg.style.filter = `blur(${blur}px)`;
-
-    let opacity = 1 - scroll / 300;
-
-    if (opacity < 0) opacity = 0;
-
-    direcionamento.style.opacity = opacity;
-
-});
-
+const apresentacao = document.querySelector(".apresentacao");
+const introducao = document.querySelector(".introducao");
+const fundos = document.querySelectorAll(".fundo");
+const opcoes = document.querySelectorAll(".opcao");
+const botaoLogin = document.querySelector(".login");
+const modal = document.querySelector("#login-modal");
 const botao = document.querySelector("#botao-contato");
+
+function atualizarScroll() {
+    let scroll = window.scrollY;
+    let blur = 10 - scroll / 50;
+    if (blur < 0) blur = 0;
+    heroimg.style.filter = `blur(${blur}px)`;
+    let opacity = 1 - scroll / 300;
+    if (opacity < 0) opacity = 0;
+    direcionamento.style.opacity = opacity;
+    let textoOpacity = scroll / 400;
+    if (textoOpacity > 1) textoOpacity = 1;
+    apresentacao.style.opacity = textoOpacity;
+    introducao.style.opacity = textoOpacity;
+
+    fundos.forEach(f => {
+        f.style.opacity = textoOpacity;
+    });
+
+    opcoes.forEach(opcao => {
+        opcao.style.opacity = textoOpacity;
+    });
+}
+window.addEventListener("scroll", atualizarScroll);
+window.addEventListener("load", atualizarScroll);
 
 botao.addEventListener("click", () => {
 
@@ -29,4 +40,26 @@ botao.addEventListener("click", () => {
         behavior: "smooth"
     });
 
+});
+
+botaoLogin.addEventListener("click", (e)=>{
+    e.preventDefault();
+    modal.style.display = "flex";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target === modal){
+        modal.style.display = "none";
+    }
+});p
+
+botaoLogin.addEventListener("click", (e)=>{
+    e.preventDefault();
+    modal.classList.add("ativo");
+});
+
+modal.addEventListener("click", (e)=>{
+    if(e.target === modal){
+        modal.style.display = "none";
+    }
 });
